1bf8deed9ef9739021aae8b99b6d3cd7f0ec0351
BCHBenoit Chabaneix <benoit.chabaneix@kellogg.northwestern.edu>
2024-04-12 18:58:40 +0000

    schools

---
+++ school-5-insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO students (
+    first_name,
+    last_name,
+    phone_number
+)
+VALUES (
+    "Jane",
+    "Doe",
+    "janedoe@example.com"
+);
---
+++ school-6-update.sql
@@ -0,0 +1,1 @@
+UPDATE students SET email = "updatemail.com" WHERE email ="janedoe@example.com";
